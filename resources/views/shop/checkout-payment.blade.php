914699b52fa96222950afe038519923f438242e5
AlbertWebs <albertmuhatia@gmail.com>
2022-08-02 14:47:32 +0300

    Update

--- resources/views/shop/checkout-payment.blade.php
+++ resources/views/shop/checkout-payment.blade.php
@@ -2,17 +2,13 @@
 @section('content')
 @foreach ($SiteSettings as $Settings)
 <main class="main">
-    <div class="page-header text-center" style="background-image: url('{{asset('theme/assets/images/page-header-bg.jpg')}}')">
-        <div class="container">
-            <h1 class="page-title">Shopping Cart<span>Shop</span></h1>
-        </div><!-- End .container -->
-    </div><!-- End .page-header -->
+  
     <nav aria-label="breadcrumb" class="breadcrumb-nav">
         <div class="container">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
                 <li class="breadcrumb-item"><a href="#">Shop</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Shopping Cart</li>
+                <li class="breadcrumb-item active" aria-current="page">Make Payment</li>
             </ol>
         </div><!-- End .container -->
     </nav><!-- End .breadcrumb-nav -->
